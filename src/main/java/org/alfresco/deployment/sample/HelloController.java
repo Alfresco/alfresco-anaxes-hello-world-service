b946246989ff044acd881087ae39f900eed7a920
Gavin Cornwell <gcornwell@gmail.com>
2018-02-06 20:03:59 +0000

    AUTH-58 & AUTH-63/AUTH-41: A proof of concept that shows how to consume an identity token across several components of the platforms
    Added properties to enable DBP configuration to be provided via environment variables.
    
    Added a method to request a JWT from the DBP.

--- src/main/java/org/alfresco/deployment/sample/HelloController.java
+++ src/main/java/org/alfresco/deployment/sample/HelloController.java
@@ -16,8 +16,26 @@
 
 package org.alfresco.deployment.sample;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -25,13 +43,41 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Configuration
 @RestController
 @RequestMapping("/hello")
 public class HelloController
 {
+    private static final Log logger = LogFactory.getLog(HelloController.class);
+    
+    private static final String CLIENT_ID_PARAM = "client_id";
+    private static final String GRANT_TYPE_PARAM = "grant_type";
+    private static final String USERNAME_PARAM = "username";
+    private static final String PASSWORD_PARAM = "password";
+    
+    private static final String CLIENT_ACTIVITI = "activiti";
+    private static final String GRANT_TYPE = "password";
+    
+    private static final String ACCESS_TOKEN_PROPERTY = "access_token";
+    
+    @Value("${alfresco.dbp.url}")
+    private String dbpUrl;
+    
+    @Value("${alfresco.dbp.username}")
+    private String dbpUsername;
+    
+    @Value("${alfresco.dbp.password}")
+    private String dbpPassword;
+    
     @Autowired
     private HelloTextRepository helloTextRepository;
+    
+    private String jwt;
 
     @RequestMapping(path = "/{key}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<HelloText> getHelloText(@PathVariable String key)
@@ -41,8 +87,14 @@ public class HelloController
         {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+        
+        // request JWT if DBP URL has been provided
+        if (this.dbpUrl != null && !this.dbpUrl.isEmpty())
+        {
+            requestAuthToken();
+        }
+        
         return new ResponseEntity<HelloText>(helloText, HttpStatus.OK);
-                
     }
 
     @RequestMapping(method = RequestMethod.POST, 
@@ -70,6 +122,75 @@ public class HelloController
         catch (Exception e)
         {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+    private void requestAuthToken()
+    {
+        // don't attempt to get another JWT if we already have one!
+        if (this.jwt == null)
+        {
+            String tokenUrl = this.dbpUrl + "/auth/realms/springboot/protocol/openid-connect/token";
+            
+            if (logger.isDebugEnabled())
+            {
+                logger.debug("Requesting JWT from " + tokenUrl);
+                logger.debug("DBP username: " + this.dbpUsername);
+                logger.debug("DBP password: " + this.dbpPassword);
+            }
+            
+            int TIMEOUT = 2000;
+           
+            try (CloseableHttpClient httpClient = HttpClientBuilder.create().build();)
+            {
+                // configure request with fairly short timeout
+                HttpPost postRequest = new HttpPost(tokenUrl);
+                RequestConfig config = RequestConfig.custom()
+                            .setSocketTimeout(TIMEOUT)
+                            .setConnectionRequestTimeout(TIMEOUT)
+                            .setConnectTimeout(TIMEOUT).build();
+                postRequest.setConfig(config);
+                
+                // build parameters
+                List<NameValuePair> params = new ArrayList<NameValuePair>();
+                params.add(new BasicNameValuePair(CLIENT_ID_PARAM, CLIENT_ACTIVITI));
+                params.add(new BasicNameValuePair(GRANT_TYPE_PARAM, GRANT_TYPE));
+                params.add(new BasicNameValuePair(USERNAME_PARAM, this.dbpUsername));
+                params.add(new BasicNameValuePair(PASSWORD_PARAM, this.dbpPassword));
+                postRequest.setEntity(new UrlEncodedFormEntity(params));
+                
+                // execute the request
+                CloseableHttpResponse response = httpClient.execute(postRequest);
+                
+                if (logger.isDebugEnabled())
+                    logger.debug("response code " + response.getStatusLine().getStatusCode());
+                
+                // extract the JWT from the response
+                if (response.getStatusLine().getStatusCode() == HttpServletResponse.SC_OK)
+                {
+                    HttpEntity entity = response.getEntity();
+                    if (entity != null)
+                    {
+                        String responseBody = EntityUtils.toString(entity);
+                        
+                        if (logger.isTraceEnabled())
+                            logger.trace("response body: " + responseBody);
+                        
+                        ObjectMapper objectMapper = new ObjectMapper();
+                        JsonNode jsonNode = objectMapper.readValue(responseBody, JsonNode.class);
+                        
+                        if (jsonNode.has(ACCESS_TOKEN_PROPERTY))
+                        {
+                            this.jwt = jsonNode.get(ACCESS_TOKEN_PROPERTY).asText();
+                            logger.info("DBP JWT: " + this.jwt);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                logger.error("Failed to obtain JWT: ", e);
+            }
         }
     }
 }
